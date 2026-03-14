cf492939799f9e8a7e3c63afcf0e7e6e0809f1c3
jao-prado <rm95221@estudante.fieb.edu.br>
2026-03-14 10:55:34 -0300

    serviços (CRUD) de usuario concluido

---
+++ src/main/java/com/itb/inf3em/studyconnect/model/services/UsuarioService.java
@@ -0,0 +1,48 @@
+package com.itb.inf3em.studyconnect.model.services;
+
+
+import com.itb.inf3em.studyconnect.model.entity.Usuario;
+import com.itb.inf3em.studyconnect.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario save(Usuario usuario){
+        usuario.setAtivo(true);
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario findById(Long id){
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado com o id" + id));
+    }
+
+
+    public Usuario update(long id, Usuario usuario){
+        Usuario UsuarioExistente = findById(id);
+        UsuarioExistente.setNome(usuario.getNome());
+        UsuarioExistente.setEmail(usuario.getEmail());
+        UsuarioExistente.setSenha(usuario.getSenha());
+        UsuarioExistente.setTipoUsuario(usuario.getTipoUsuario());
+        UsuarioExistente.setAtivo(usuario.isAtivo());
+        return usuarioRepository.save(UsuarioExistente);
+    }
+
+    public void delete(long id){
+        Usuario UsuarioExistente = findById(id);
+        usuarioRepository.delete(UsuarioExistente);
+    }
+
+}
